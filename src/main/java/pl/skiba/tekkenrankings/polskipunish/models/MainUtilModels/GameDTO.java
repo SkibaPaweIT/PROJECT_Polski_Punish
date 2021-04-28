7967c9129a1ad955033b4d2447929f0a036bac09
SkibaPawelT <skibapawelt@gmail.com>
2021-04-28 21:19:13 +0200

    Hot-Fixing stuff

---
+++ src/main/java/pl/skiba/tekkenrankings/polskipunish/models/MainUtilModels/GameDTO.java
@@ -0,0 +1,23 @@
+package pl.skiba.tekkenrankings.polskipunish.models.MainUtilModels;
+
+public class GameDTO {
+
+    private Long id;
+    private String gameName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+}
