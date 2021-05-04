2c2d0fffa3abd9f24f2692204996d2184135ad16
SkibaPawelT <skibapawelt@gmail.com>
2021-05-04 13:11:05 +0200

    working with importing test data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO post_model(highlighted, text, title, url) VALUES
+(true , 'text1' , 'title1' , 'url1'),
+(true , 'text2' , 'title2' , 'url2'),
+(false , 'text3' , 'title3' , 'url3'),
+(false , 'text4' , 'title4' , 'url4');
